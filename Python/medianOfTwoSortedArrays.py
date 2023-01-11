b11d0d4b58514fe448eaae155fbf5ca91c3523b4
dostl <dostl@kth.se>
2023-01-12 00:56:35 +0100

    added medianOfTwoSortedArrays

---
+++ Python/medianOfTwoSortedArrays.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        
+        mergedArray = nums1+nums2
+        mergedArray.sort()
+        if(len(mergedArray)%2 != 0):       
+            return mergedArray[len(mergedArray)/2]
+        if(len(mergedArray)%2 == 0): 
+            return (float(mergedArray[len(mergedArray)/2-1]+mergedArray[len(mergedArray)/2])/2)
