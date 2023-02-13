3c7d1fa1f15e6f21a1e272f02c89b6ce709cf029
dostl <dostl@kth.se>
2023-02-13 21:06:46 +0100

    fix: wordPattern passes all tests

--- Java/wordPattern.java
+++ Java/wordPattern.java
@@ -1,27 +1,26 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] inputArr = pattern.split("(?!^)");
-        String[] patternArr = s.split(" ");
-
-        Map<String, String> map = new HashMap<String, String>();
-
-        for (int i = 0; i <= inputArr.length - 1; i++) {
-            if (map.get(inputArr[i]) == null) {
-                map.put(inputArr[i], patternArr[i]);
-            }
-
-        }
-
-        String patternChecker = "";
-        for (int i = 0; i <= inputArr.length - 1; i++) {
-            patternChecker += map.get(inputArr[i]) + " ";
-        }
-
-        if (patternChecker.trim().equals(s) && (!map.get("a").equals(map.get("b")))) {
-            return true;
-        } else {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) {
             return false;
         }
+
+        Map<Character, String> map = new HashMap<>();
+        Set<String> usedWords = new HashSet<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            if (!map.containsKey(c)) {
+                if (usedWords.contains(words[i])) {
+                    return false;
+                }
+                map.put(c, words[i]);
+                usedWords.add(words[i]);
+            } else if (!map.get(c).equals(words[i])) {
+                return false;
+            }
+        }
+
+        return true;
     }
-    // Problem description: https://leetcode.com/problems/word-pattern/description/
 }
