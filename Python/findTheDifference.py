0ea4e8929b8197f89fb4cc170d5454204a74f4c4
dostl <dostl@kth.se>
2023-02-16 23:23:06 +0100

    added findTheDifference

---
+++ Python/findTheDifference.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        sList = list(s)
+        tList = list(t)
+        sDict = {}
+        tDict = {}
+
+        def counter(xDict, xList):
+            for element in xList:
+                xDict[element] = xList.count(element)
+
+        def count():
+            counter(sDict, sList)
+            counter(tDict, tList)
+        count()
+
+        for element in tDict:
+            if element in sDict and tDict[element] != sDict[element]:
+                return element
+            elif element not in sDict:
+                return element
+            
+#Problem description: https://leetcode.com/problems/find-the-difference/description/
